9a54fe73247bfd2a71eb13b489f95f23fca4bf8a
Archangel354 <rduncan354@gmail.com>
2018-10-01 10:54:35 -0400

    2nd commit that updates JsonUtils.java with data fields and image name

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,55 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class JsonUtils {
 
+    public static final String NAME = "name";
+    public static final String MAIN_NAME = "mainName";
+    public static final String ALSO_KNOWN_AS = "alsoKnownAs";
+    public static final String PLACE_OF_ORIGIN = "placeOfOrigin";
+    public static final String DESCRIPTION = "description";
+    public static final String IMAGE = "image";
+    public static final String INGREDIENTS = "ingredients";
+
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        Sandwich sandwich = new Sandwich();
+        try {
+
+            JSONObject rootObject = new JSONObject(json);
+            JSONObject nameObject = rootObject.getJSONObject(NAME);
+            sandwich.setMainName(nameObject.getString(MAIN_NAME));
+            sandwich.setAlsoKnownAs(getJsonArrayAsList(nameObject.getJSONArray(ALSO_KNOWN_AS)));
+            sandwich.setPlaceOfOrigin(rootObject.getString(PLACE_OF_ORIGIN));
+            sandwich.setDescription(rootObject.getString(DESCRIPTION));
+            sandwich.setImage(rootObject.getString(IMAGE));
+            sandwich.setIngredients(getJsonArrayAsList(rootObject.getJSONArray(INGREDIENTS)));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return sandwich;
     }
+
+    public static ArrayList<String> getJsonArrayAsList(JSONArray array) {
+        ArrayList<String> list = new ArrayList<>();
+        try {
+            for (int i = 0; i < array.length(); i++) {
+                list.add(array.getString(i));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.i(String.valueOf(list), "getJsonArrayAsList: ");
+        return list;
+    }
+
 }
